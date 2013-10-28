212efbee61d503419c15b31a6b211c1343b7dbf5
Nimthora <Nimthora@gmail.com>
2013-10-28 21:42:52 +0100

    Removal of useless classes

--- entities/TestBlock.java
+++
@@ -1,9 +0,0 @@
-package com.auditory.entities;
-
-import com.auditory.Entity;
-
-public class TestBlock extends Entity {
-	public TestBlock(float x, float y, float z) {
-		super(x, y, z);
-	}
-}
--- util/Test.java
+++
@@ -1,9 +0,0 @@
-package com.auditory.util;
-
-public class Test {
-	public static void main(String[] args) {
-		float a = -359.2231f;
-		float b = 2f;
-		System.out.println((a - b) % 360);
-	}
-}
